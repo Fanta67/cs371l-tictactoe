3b0d4b10239530bb8f12cd37720b22921ec1255a
billyvo <billyvo@utexas.edu>
2020-06-21 23:19:36 -0500

    first commit

---
+++ cs371l-tictactoe/cs371l-tictactoe/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  Filename: ViewController.swift
+//  EID: bv5433
+//  Course: CS371L
+//
+//  Created by Billy Vo on 6/21/20.
+//  Copyright © 2020 billyvo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
