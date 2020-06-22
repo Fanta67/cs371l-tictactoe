274ba1aca088bc69c6de1cbf1a2d9a89f429894f
Dylan Kan <dylan.kan67@utexas.edu>
2020-06-22 00:05:28 -0500

    added postgame screen and match hist table

---
+++ cs371l-tictactoe/PostGameVC.swift
@@ -0,0 +1,30 @@
+//
+//  PostGameVC.swift
+//  cs371l-tictactoe
+//
+//  Created by Dylan Kan on 6/22/20.
+//  Copyright © 2020 billyvo. All rights reserved.
+//
+
+import UIKit
+
+class PostGameVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
