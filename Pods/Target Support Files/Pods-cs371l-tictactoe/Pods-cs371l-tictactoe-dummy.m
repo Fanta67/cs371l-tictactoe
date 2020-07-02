31572bcc9ea9ff0e00cf6f2be9c5e1f045916497
billyvo <billyvo@utexas.edu>
2020-07-02 14:45:33 -0500

    database pt1

---
+++ Pods/Target Support Files/Pods-cs371l-tictactoe/Pods-cs371l-tictactoe-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_cs371l_tictactoe : NSObject
+@end
+@implementation PodsDummy_Pods_cs371l_tictactoe
+@end
--- cs371l-tictactoe/GameVC.swift
+++ cs371l-tictactoe/GameVC.swift
@@ -9,21 +9,52 @@
 //
 
 import UIKit
+import Firebase
+import Foundation
 
 class GameVC: UIViewController {
+    
+    let queue = DispatchQueue(label: "q1", qos: .userInitiated)
+    
+    var inviteCode: String = ""
+    var playerID: String = ""
+    var gameRef: DatabaseReference = Database.database().reference()
 
+    @IBOutlet weak var blButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        gameRef = Database.database().reference().child("game/\(inviteCode)")
     }
     
     override func viewWillAppear(_ animated: Bool) {
+        /*
         if(settings[0].value(forKeyPath: "isOn") as! Bool) {
             overrideUserInterfaceStyle = .dark
         } else {
             overrideUserInterfaceStyle = .light
         }
+         */
     }
+    
+    func game() {
+        let playerTurn = gameRef.value(forKey: "playerTurn") as! Int
+        if gameRef.value(forKey: "player\(playerTurn)") as! String == playerID {
+            allowTurn()
+        } else {
+            disallowTurn()
+        }
+    }
+    
+    func allowTurn() {
+    }
+        
+    func disallowTurn() {
+        // make board unclickable
+        blButton.isEnabled = false
+    }
+}
 
+class TicTacToe {
+    
 }
