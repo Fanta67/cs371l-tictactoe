d76bdbe024055e0dc78219efe0aae8878b484935
billyvo <billyvo@utexas.edu>
2020-07-09 12:11:12 -0500

    postgame core

--- cs371l-tictactoe/PostGameVC.swift
+++ cs371l-tictactoe/PostGameVC.swift
@@ -16,7 +16,16 @@ class PostGameVC: UIViewController, UIActivityItemSource {
     
     var match: NSManagedObject!
     
-    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var boardImageView: UIImageView!
+    @IBOutlet weak var panel1: UIImageView!
+    @IBOutlet weak var panel2: UIImageView!
+    @IBOutlet weak var panel3: UIImageView!
+    @IBOutlet weak var panel4: UIImageView!
+    @IBOutlet weak var panel5: UIImageView!
+    @IBOutlet weak var panel6: UIImageView!
+    @IBOutlet weak var panel7: UIImageView!
+    @IBOutlet weak var panel8: UIImageView!
+    @IBOutlet weak var panel9: UIImageView!
     @IBOutlet weak var label: UILabel!
     var leftBarButton: UIBarButtonItem!
     
@@ -25,18 +34,41 @@ class PostGameVC: UIViewController, UIActivityItemSource {
         
         label.text = match.value(forKeyPath: "whoWon") as? String
         let gameImageData = match.value(forKeyPath: "gameImage") as! Data
-        imageView.image = UIImage(data: gameImageData)
+        
     }
     
     override func viewWillAppear(_ animated: Bool) {
         if(settings[0].value(forKeyPath: "isOn") as! Bool) {
             overrideUserInterfaceStyle = .dark
             self.navigationController?.navigationBar.barTintColor = .black
+            boardImageView.image = UIImage(data: "board-10pix-white-transparent")
         } else {
             overrideUserInterfaceStyle = .light
             self.navigationController?.navigationBar.barTintColor = .white
+            boardImageView.image = UIImage(data: "board-10pix-white-transparent")
         }
     }
+    
+    func setImagesFromCoreData() {
+        let gameStateArray = match.value(forKey: "gameState") as! [Int]
+        let imageViewArray = [
+            panel1, panel2, panel3,
+            panel4, panel5, panel6,
+            panel7, panel8, panel9,
+        ]
+        for i in 0 ..< gameStateArray.count {
+            if (gameStateArray[i] == 1) {
+                panelArray[i]!.image = UIImage(named: "x.png")!
+            } else if (gameStateArray[i] == 2) {
+                panelArray[i]!.image = UIImage(named: "o.png")!
+                panelArray[i]!.image = UIImage(named: "board-10pix-white-transparent")
+
+            } else {
+                panelArray[i]!.image = nil
+            }
+        }
+    }
+
     
     @objc func segueToMainMenu() {
         performSegue(withIdentifier: "PostgameToMainMenuSegue", sender: nil)
@@ -50,14 +82,14 @@ class PostGameVC: UIViewController, UIActivityItemSource {
 
     /// Creates an UIActiviyViewController to share an image of the match's board.
     @IBAction func shareButtonPressed(_ sender: Any) {
-        let activityViewController = UIActivityViewController(activityItems: [imageView.image!, self], applicationActivities: nil)
+        let activityViewController = UIActivityViewController(activityItems: [boardImageView.image!, self], applicationActivities: nil)
         activityViewController.popoverPresentationController?.sourceView = self.view
         present(activityViewController, animated: true, completion: nil)
     }
     
     /// Displays the board image as the thumbnail when sharing.
     func activityViewControllerLinkMetadata(_ activityViewController: UIActivityViewController) -> LPLinkMetadata? {
-        let image = imageView.image!
+        let image = boardImageView.image!
         let imageProvider = NSItemProvider(object: image)
         let metadata = LPLinkMetadata()
         let result = match.value(forKeyPath: "whoWon") as? String
